0bb2d40065817295079fe4aabad7c40ed8c81237
Narendra <102171116+Nairy1729@users.noreply.github.com>
2024-11-01 15:38:15 +0530

    Add project files.

---
+++ DBfirst/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace DBfirst.Models
+{
+    //Plain Old CLR Object
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+
+}
---
+++ DBfirst/Models/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DBfirst.Models
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
